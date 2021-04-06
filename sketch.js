5e3b032d507291c416bba55c0fc7041d56dae3e5
Sanjivajha <43417755+Sanjivajha@users.noreply.github.com>
2021-04-06 13:38:41 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -2,8 +2,8 @@ var dog,sadDog,happyDog;
 
 
 function preload(){
-  sadDog=loadImage("Images/Dog.png");
-  happyDog=loadImage("Images/happy dog.png");
+  sadDog=loadImage("Dog.png");
+  happyDog=loadImage("happy dog.png");
 }
 
 function setup() {
